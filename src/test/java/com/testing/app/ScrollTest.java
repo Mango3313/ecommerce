d57f164a3389a9dbdcac11b16f4bbf7284034b75
Mango3313 <daft_noob3313@live.com>
2022-08-17 10:36:37 -0500

    Initial commit

---
+++ src/test/java/com/testing/app/ScrollTest.java
@@ -0,0 +1,5 @@
+package com.testing.app;
+
+public class ScrollTest {
+    
+}
---
+++ src/test/java/com/testing/app/UserRegistry.java
@@ -0,0 +1,18 @@
+package com.testing.app;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UserRegistry 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        
+    }
+}
