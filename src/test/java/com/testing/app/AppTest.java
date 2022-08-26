55674d16765cb8155edf66afef18e8256b084bbc
Mango3313 <daft_noob3313@live.com>
2022-08-25 21:36:31 -0500

    Updates

--- src/test/java/com/testing/app/AppTest.java
+++ src/test/java/com/testing/app/AppTest.java
@@ -3,18 +3,28 @@ import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.text.DecimalFormat;
 import java.time.Duration;
+import java.util.Locale;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.openqa.selenium.By;
-//Añadí librerias faltantes, no reconoce algunas (Lidia)
+import org.openqa.selenium.Keys;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -23,6 +33,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class AppTest {
     // Declare WebDriver
     private ChromeDriver driver;
+    int contador = 1;
 
     @BeforeSuite
     public void configSuite() {
@@ -45,31 +56,37 @@ public class AppTest {
 
     // User register
     @Test
-    public void TC_001_CorrectUserRegister() throws InterruptedException {
+    public void TC_001_CorrectUserRegister() throws InterruptedException { 
         driver.get("http://automationpractice.com/index.php");
+        FakeValuesService fakeValuesService = new FakeValuesService(
+                new Locale("es-MX"), new RandomService()
+                );
+        String email = fakeValuesService.bothify("????##@gmail.com");
         Assert.assertEquals(driver.getTitle(), "My Store");
+        contador = contador + 1;
+        Actions actions = new Actions(driver);
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000));
 
+        
         // Main page
-        WebElement buttonSignIn = driver.findElement(By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[1]/a"));
-        buttonSignIn.click();
+        WebElement buttonSignIn = driver.findElement(By.xpath("/html/body/div[1]/div[1]/header/div[2]/div/div/nav/div[1]/a"));
         wait.until(ExpectedConditions.elementToBeClickable(buttonSignIn)).click();
-        //
+
         WebElement inputEmail = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/form/div/div[2]/input"));
-        inputEmail.sendKeys("ivaanerygarcia@gmail.com");
+        inputEmail.sendKeys(email);
 
         WebElement buttonCreateAccount = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/form/div/div[3]/button"));
-        buttonCreateAccount.click();
-        wait.until(ExpectedConditions.elementToBeClickable(buttonCreateAccount)).click();
-        
+        wait.until(ExpectedConditions.elementToBeClickable(buttonCreateAccount)).click();;
+        //Wait for changes pa que jale jaja 
+        Thread.sleep(7000);
         // Personal Information Page
         // Title
         WebElement inputGender1, inputGender2;
-        inputGender1 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[1]/div[1]/div[1]/label/div/span/input"));
-        /** /html/body/div/div[2]/div/div[3]/div/div/form/div[1]/div[1]/div[1]/label/div/span/input */
-        inputGender1.click();
-        inputGender2 = driver.findElement(By.id("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[1]/div[1]/div[2]/label/div/span/input"));
-        inputGender2.click();
+        inputGender1 = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/form/div[1]/div[1]/div[1]/label/div/span/input"));
+        actions.moveToElement(inputGender1).click().perform();
+        inputGender2 = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/form/div[1]/div[1]/div[2]/label/div/span/input"));
+        actions.moveToElement(inputGender2).click().perform();
 
         // Name
         WebElement inputName = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[1]/div[2]/input"));
@@ -113,69 +130,121 @@ public class AppTest {
         inputAddress.sendKeys("221B Bakerstreet");
 
         // Address2
-        WebElement inputAddress2 = driver
-                .findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[5]/input"));
+        WebElement inputAddress2 = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[5]/input"));
         inputAddress2.sendKeys("Another address");
 
         // City
-        WebElement inputCity = driver
-                .findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[6]/input"));
+        WebElement inputCity = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[6]/input"));
         inputCity.sendKeys("New York");
 
+
         // State
-        WebElement state = driver
-                .findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[7]/div/span"));
-        state.click();
+        WebElement state = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[7]/div/select"));
+        Select select = new Select(state);
+        select.selectByValue("32");
 
         // CP
-        WebElement inputPC = driver
-                .findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[8]/input"));
+        WebElement inputPC = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[8]/input"));
         inputPC.sendKeys("87298");
 
         // Country
-        WebElement country = driver
-                .findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[9]/div/span"));
-        country.click();
+        WebElement country = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[9]/div/select"));
+        Select select2 = new Select(country);
+        select2.selectByValue("21");
 
         // Additional Information
-        WebElement inputAI = driver
-                .findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[10]/textarea"));
+        WebElement inputAI = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[10]/textarea"));
         inputAI.sendKeys("Here can you write more info.");
 
         // Phone
-        WebElement inputPhone = driver
-                .findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[12]/input"));
+        WebElement inputPhone = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[12]/input"));
         inputPhone.sendKeys("0123456789");
 
         // Alias address
-        WebElement inputAliasAddress = driver
-                .findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[14]/input"));
+        WebElement inputAliasAddress = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[2]/p[14]/input"));
         inputAliasAddress.sendKeys("My address.");
 
         // Register button
-        WebElement buttonRegister = driver
-                .findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[4]/button"));
+        WebElement buttonRegister = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[4]/button"));
+        buttonRegister.click();
+    }
+
+    // Incorrect User  register (Empty fields)
+    @Test
+    public void TC_002_InorrectUserRegister() throws InterruptedException { 
+        driver.get("http://automationpractice.com/index.php");
+        Assert.assertEquals(driver.getTitle(), "My Store");
+        FakeValuesService fakeValuesService = new FakeValuesService(
+                new Locale("es-MX"), new RandomService()
+                );
+        String email = fakeValuesService.bothify("????##@gmail.com");
+        contador = contador + 1;
+        Actions actions = new Actions(driver);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000));
+        // Main page
+        WebElement buttonSignIn = driver.findElement(By.xpath("/html/body/div[1]/div[1]/header/div[2]/div/div/nav/div[1]/a"));
+        wait.until(ExpectedConditions.elementToBeClickable(buttonSignIn)).click();
+
+        WebElement inputEmail = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/form/div/div[2]/input"));
+        inputEmail.sendKeys(email);
+
+        WebElement buttonCreateAccount = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/div[1]/form/div/div[3]/button"));
+        wait.until(ExpectedConditions.elementToBeClickable(buttonCreateAccount)).click();;
+        //Wait for changes pa que jale jaja 
+        Thread.sleep(7000);
+        // Personal Information Page
+        // Register button
+        WebElement buttonRegister = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/form/div[4]/button"));
         buttonRegister.click();
     }
 
     // User login
     @Test
-    public void TC_002_CorrectUserLogin() throws InterruptedException {
+    public void TC_003_CorrectUserLogin() throws InterruptedException {
         driver.get("http://automationpractice.com/index.php");
         Assert.assertEquals(driver.getTitle(), "My Store");
+        FakeValuesService fakeValuesService = new FakeValuesService(
+                new Locale("es-MX"), new RandomService()
+                );
+        String email = fakeValuesService.bothify("????##@gmail.com");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000));
         
-         // Main page
+        // Main page
         WebElement buttonSignIn = driver.findElement(By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[1]/a"));
-        buttonSignIn.click();
+        wait.until(ExpectedConditions.elementToBeClickable(buttonSignIn)).click();
 
         //Second page
         //Email
         WebElement inputEmail = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/div[2]/form/div/div[1]/input"));
-        inputEmail.sendKeys("ivaanerygarcia@gmail.com");
+        inputEmail.sendKeys(email);
 
         //Password
         WebElement inputPass = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/div[2]/form/div/div[2]/span/input"));
         inputPass.sendKeys("12345");
+        
+        //Login button
+        WebElement buttonLogin = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button"));
+        buttonLogin.click();
+     
+    }
+
+    // Incorrect User login (Empty fields)
+    @Test
+    public void TC_004_CorrectUserLogin() throws InterruptedException {
+        driver.get("http://automationpractice.com/index.php");
+        Assert.assertEquals(driver.getTitle(), "My Store");
+        FakeValuesService fakeValuesService = new FakeValuesService(
+                new Locale("es-MX"), new RandomService()
+                );
+        String email = fakeValuesService.bothify("????##@gmail.com");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000));
+
+         // Main page
+        WebElement buttonSignIn = driver.findElement(By.xpath("/html/body/div/div[1]/header/div[2]/div/div/nav/div[1]/a"));
+        wait.until(ExpectedConditions.elementToBeClickable(buttonSignIn)).click();
+
+        //Second page
         
         //Login button
         WebElement buttonLogin = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button"));
@@ -210,25 +279,31 @@ public class AppTest {
         wait.until(ExpectedConditions.elementToBeClickable(button)).click();
 
         WebElement search = driver
-        .findElement(By.xpath("html/body/div/div[1]/header/div[3]/div/div/div[2]/form/input[4]"));
+        .findElement(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[2]/form/input[4]"));
         search.sendKeys("dress");
-        wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+        search.sendKeys(Keys.ENTER);
+        search.clear();
+        //wait.until(ExpectedConditions.elementToBeClickable(button)).click();
 
         WebElement search2Element = driver
         .findElement(By.xpath("html/body/div/div[1]/header/div[3]/div/div/div[2]/form/input[4]"));
         search2Element.sendKeys("jeans");
-        wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+        search2Element.sendKeys(Keys.ENTER);
+        search2Element.clear();
+        //wait.until(ExpectedConditions.elementToBeClickable(button)).click();
 
-       wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+       //wait.until(ExpectedConditions.elementToBeClickable(button)).click();
         WebElement search3Element = driver
         .findElement(By.xpath("html/body/div/div[1]/header/div[3]/div/div/div[2]/form/input[4]"));
-        search.sendKeys("BloUsE");
+        //Falla aqui, no se porque no detecta el input de busqueda
+        wait.until(ExpectedConditions.elementToBeClickable(search)).sendKeys("BloUsE");
         wait.until(ExpectedConditions.elementToBeClickable(button)).click();
 
         WebElement search4Element = driver
         .findElement(By.xpath("html/body/div/div[1]/header/div[3]/div/div/div[2]/form/input[4]"));
         search.sendKeys("");
-        wait.until(ExpectedConditions.elementToBeClickable(button)).click();
+        search.sendKeys(Keys.ENTER);
+        //wait.until(ExpectedConditions.elementToBeClickable(button)).click();
         
     }
 
@@ -315,6 +390,50 @@ public class AppTest {
 
     @Test
     public void TC_007_BuyCart() {
+        driver.get("http://automationpractice.com/index.php");
+        //Set WebDriverWait, Actions and Decimal format for numbers
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(10000));
+        Actions actions = new Actions(driver);
+        //Check if the page name is correct
+        Assert.assertEquals(driver.getTitle(), "My Store");
+        //Hover on first item
+        WebElement addToCartI = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div/div[1]/ul[1]/li[1]/div"));
+        actions.moveToElement(addToCartI).perform();
+        //Add first item into cart
+        WebElement addToCartB = driver.findElement(By.xpath("//html/body/div/div[2]/div/div[2]/div/div[1]/ul[1]/li[1]/div/div[2]/div[2]/a[1]"));
+        wait.until(ExpectedConditions.elementToBeClickable(addToCartB)).click();
+        //Click continue shopping
+        wait.until(
+                ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[2]/div[4]/span")))
+                .click();        
+        //Hover second item
+        WebElement addToCartI2 = driver.findElement(By.xpath("/html/body/div/div[2]/div/div[2]/div/div[1]/ul[1]/li[2]/div"));
+        actions.moveToElement(addToCartI2).perform();
+        //Add second item
+        WebElement addToCartB2 = driver.findElement(By.xpath("//*[@id='homefeatured']/li[2]/div/div[2]/div[2]/a[1]/span"));
+        wait.until(ExpectedConditions.elementToBeClickable(addToCartB2)).click();
+        //Gotocart
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("/html/body/div/div[1]/header/div[3]/div/div/div[4]/div[1]/div[2]/div[4]/a")
+        )).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("/html/body/div/div[2]/div/div[3]/div/p[2]/a[1]")
+        )).click();  
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form")));
+        
+        WebElement mail = driver.findElement(
+                By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/div[1]/input")
+        );
+        mail.sendKeys("fuentesf.jorgealan@gmail.com");
+        WebElement pass = driver.findElement(
+                By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/div[2]/span/input")
+        );
+        pass.sendKeys("123345");
+        driver.findElement(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/div[2]/form/div/p[2]/button")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div/div[2]/div/div[3]/div/div/form")));
 
     }
 
